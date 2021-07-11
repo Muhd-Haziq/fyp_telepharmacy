c7d0a94e83ef30c737cd23f74e7f7a26cc1563d7
Daryl <darylgialolo@gmail.com>
2021-07-11 17:33:03 +0800

    Added getOTCDrugsInStockPopular.php Added Directory for Drug Images

--- patient/getOTCDrugs.php
+++ patient/getOTCDrugs.php
@@ -7,20 +7,21 @@ $drugs = array();
     
 // SQL Statement to get Drugs
 $drugsQuery = "SELECT * " . 
-             "FROM drug " . 
-             "WHERE drug_type = 'Over-the-counter' ";
+              "FROM drug " . 
+              "WHERE drug_type = 'Over-the-counter' ";
+
+//echo $drugsQuery;
 
 // Retrieve Drug Rows
 $result = mysqli_query($link, $drugsQuery) or die(mysqli_error($link));
 
-if ($row = mysqli_fetch_assoc($result)) {
+while ($row = mysqli_fetch_assoc($result)) {
     $drugs[] = $row;
 }
 
 if (!empty($drugs)) {
     $isEmpty = false;
 }
-
 
 echo json_encode( array("drugs" => $drugs, "isEmpty" => $isEmpty) );
 mysqli_close($link);
--- patient/getOTCDrugsInStockPopular.php
+++ patient/getOTCDrugsInStockPopular.php
@@ -9,7 +9,7 @@ $drugs = array();
 $drugsQuery = "SELECT * " . 
               "FROM drug " . 
               "WHERE drug_in_stock != 0 AND drug_type = 'Over-the-counter' " .
-              "ORDER BY drug_date_added ASC ";
+              "ORDER BY drug_sales DESC ";
 
 // Retrieve Drug Rows
 $result = mysqli_query($link, $drugsQuery) or die(mysqli_error($link));
